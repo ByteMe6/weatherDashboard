12eb81fbcb152c29d7681b86bcd66cb7a4a3c286
ByteMe6 <tsapiy.yurka@gmail.com>
2025-07-31 20:23:06 +0300

    header
    css, html. and animations. fully ready, but no functionla header

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,8 +1,37 @@
-import React from 'react';
-import style from "./Header.module.scss"
+import React, { useEffect } from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import AOS from "aos";
+import "aos/dist/aos.css";
+import style from "./Header.module.scss";
+import cont from "../Container/Container.module.scss";
 
 export function Header() {
+  useEffect(() => {
+    AOS.init({ duration: 800, once: true });
+  }, []);
+
   return (
-    <header>GO TO FUCK!!! BIATCH</header>
+    <header className={cont.container}>
+      <nav className={style.nav} data-aos="fade-down">
+        <img src="./logo.png" alt="" data-aos="zoom-in" data-aos-delay="100" />
+        <ul className={style.links}>
+          <li className={style.linkListItem} data-aos="fade-up" data-aos-delay="200">Who we are</li>
+          <li className={style.linkListItem} data-aos="fade-up" data-aos-delay="300">Contacts</li>
+          <li className={style.linkListItem} data-aos="fade-up" data-aos-delay="400">Menu</li>
+        </ul>
+      </nav>
+      <ul className={style.logInHeader}>
+        <li className={style.LogInHeaderItem} data-aos="fade-left" data-aos-delay="500">
+          <button type="button" className="btn btn-warning">
+            Warning
+          </button>
+        </li>
+        <li className={style.LogInHeaderItem} data-aos="fade-left" data-aos-delay="600">
+          <button type="button" className="btn">
+            <img src="./user.png" alt="" />
+          </button>
+        </li>
+      </ul>
+    </header>
   );
-};
+}
