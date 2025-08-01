bf1879b3a1d99f45a2e40f86b4be144cabc77fb0
ByteMe6 <tsapiy.yurka@gmail.com>
2025-08-01 18:38:33 +0300

    login
    it won't work because api is in the .env

--- src/App.jsx
+++ src/App.jsx
@@ -5,11 +5,29 @@ import { Weather } from "./Components/Weather/Weather";
 import { News } from "./Components/News/news";
 import { Slider } from "./Components/Slider/Slider";
 import { Footer } from "./Components/Footer/Footer";
+import { useAuth } from "./hooks/useAuth";
+import React, { useState } from "react";
+
 
 function App() {
+  const { user, loading, signInWithGoogle, logout } = useAuth();
+  const [login, setLogin] = useState(false);
+
+  React.useEffect(() => {
+    setLogin(!!user);
+  }, [user]);
+
+  if (loading) {
+    return <div>Loading...</div>;
+  }
   return (
     <>
-      <Header />
+      <Header
+        user={user}
+        login={login}
+        onLogin={signInWithGoogle}
+        onLogout={logout}
+      />
       <main>
         <Hero></Hero>
         <Weather></Weather>
--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -5,9 +5,9 @@ import "aos/dist/aos.css";
 import style from "./Header.module.scss";
 import cont from "../Container/Container.module.scss";
 
-export function Header() {
+export function Header({ user, login, onLogin, onLogout }) {
   useEffect(() => {
-    AOS.init({ duration: 800, once: true });
+    AOS.init({ duration: 500, once: true });
   }, []);
 
   return (
@@ -15,22 +15,70 @@ export function Header() {
       <nav className={style.nav} data-aos="fade-down">
         <img src="./logo.png" alt="" data-aos="zoom-in" data-aos-delay="100" />
         <ul className={style.links}>
-          <li className={style.linkListItem} data-aos="fade-up" data-aos-delay="200">Who we are</li>
-          <li className={style.linkListItem} data-aos="fade-up" data-aos-delay="300">Contacts</li>
-          <li className={style.linkListItem} data-aos="fade-up" data-aos-delay="400">Menu</li>
+          <li
+            className={style.linkListItem}
+            data-aos="fade-up"
+            data-aos-delay="200"
+          >
+            Who we are
+          </li>
+          <li
+            className={style.linkListItem}
+            data-aos="fade-up"
+            data-aos-delay="300"
+          >
+            Contacts
+          </li>
+          <li
+            className={style.linkListItem}
+            data-aos="fade-up"
+            data-aos-delay="400"
+          >
+            Menu
+          </li>
         </ul>
       </nav>
       <ul className={style.logInHeader}>
-        <li className={style.LogInHeaderItem} data-aos="fade-left" data-aos-delay="500">
-          <button type="button" className="btn btn-warning">
-            Warning
-          </button>
-        </li>
-        <li className={style.LogInHeaderItem} data-aos="fade-left" data-aos-delay="600">
-          <button type="button" className="btn">
-            <img src="./user.png" alt="" />
-          </button>
-        </li>
+        {!login ? (
+          <li
+            className={style.LogInHeaderItem}
+            data-aos="fade-left"
+            data-aos-delay="500"
+          >
+            <button type="button" className="btn btn-warning" onClick={onLogin}>
+              Sign In with Google
+            </button>
+          </li>
+        ) : (
+          <>
+            <li
+              className={style.LogInHeaderItem}
+              data-aos="fade-left"
+              data-aos-delay="600"
+            >
+              <button
+                type="button"
+                className="btn btn-warning me-2"
+                onClick={onLogout}
+              >
+                Logout
+              </button>
+            </li>
+            <li
+              className={style.LogInHeaderItem}
+              data-aos="fade-left"
+              data-aos-delay="700"
+            >
+              <button type="button" className="btn">
+                <img
+                  src="./user.png"
+                  alt="User Avatar"
+                  style={{ width: "32px", height: "32px", borderRadius: "50%" }}
+                />
+              </button>
+            </li>
+          </>
+        )}
       </ul>
     </header>
   );
