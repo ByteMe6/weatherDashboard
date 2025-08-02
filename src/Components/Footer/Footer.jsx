4b858e7bf92fac197c3f746a54b1961dabe3538e
ByteMe6 <tsapiy.yurka@gmail.com>
2025-08-02 19:51:09 +0300

    Update Footer.jsx

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -14,42 +14,55 @@ export function Footer() {
   }, []);
 
   return (
-    <footer
-      className={styles.footer}
-      data-aos="fade-up"
-    >
-      <div className={cont.container} style={{ display: "flex", justifyContent: "flex-start", alignItems: "center", gap: "30px"}}>
-      <img src="./logo.png" alt="" data-aos="zoom-in" data-aos-delay="100" />
-      <ul className={styles.footerItemList} data-aos="fade-right" data-aos-delay="200">
-        <li className={styles.footerItemListItem}>
-          <h6>Adress</h6>
-        </li>
-        <li className={styles.footerItemListItem}>
-          <p>
-            Svobody str. 35
-            <br />
-            Kyiv
-            <br /> Ukraine
-          </p>
-        </li>
-      </ul>
+    <footer className={styles.footer} data-aos="fade-up">
+      <div
+        className={cont.container}
+        style={{
+          display: "flex",
+          justifyContent: "flex-start",
+          alignItems: "center",
+          gap: "30px",
+        }}
+      >
+        <img src="./logo.png" alt="" data-aos="zoom-in" data-aos-delay="100" />
+        <ul
+          className={styles.footerItemList}
+          data-aos="fade-right"
+          data-aos-delay="200"
+        >
+          <li className={styles.footerItemListItem}>
+            <h6>Adress</h6>
+          </li>
+          <li className={styles.footerItemListItem}>
+            <p>
+              Svobody str. 35
+              <br />
+              Kyiv
+              <br /> Ukraine
+            </p>
+          </li>
+        </ul>
 
-      <ul className={styles.footerItemList} data-aos="fade-left" data-aos-delay="300">
-        <li className={styles.footerItemListItem}>
-          <h6>Contact us</h6>
-        </li>
-        <li className={styles.footerItemListItem}>
-          <ul>
-            <li data-aos="zoom-in" data-aos-delay="400">
-              <FaInstagram />
-            </li>
-            <li data-aos="zoom-in" data-aos-delay="500">
-              <FaSquareFacebook />
-            </li>
-            <li data-aos="zoom-in" data-aos-delay="600">
-              <FaWhatsappSquare />
-            </li>
-          </ul>
+        <ul
+          className={styles.footerItemList}
+          data-aos="fade-left"
+          data-aos-delay="300"
+        >
+          <li className={styles.footerItemListItem}>
+            <h6>Contact us</h6>
+          </li>
+          <li className={styles.footerItemListItem}>
+            <ul>
+              <li data-aos="zoom-in" data-aos-delay="400">
+                <FaInstagram />
+              </li>
+              <li data-aos="zoom-in" data-aos-delay="500">
+                <FaSquareFacebook />
+              </li>
+              <li data-aos="zoom-in" data-aos-delay="600">
+                <FaWhatsappSquare />
+              </li>
+            </ul>
           </li>
         </ul>
       </div>
