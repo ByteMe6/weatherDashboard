28c4484afd87333ab0dafa0b51824a7e641b2a1e
ByteMe6 <tsapiy.yurka@gmail.com>
2025-08-12 18:40:12 +0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: process.env.NODE_ENV === 'production' ? '/weatherDashboard/' : '/', // eslint-disable-line no-undef
+  base: process.env.V === 'true'
+    ? '/'
+    : (process.env.NODE_ENV === 'production' ? '/weatherDashboard/' : '/'),
   define: {
     global: 'globalThis',
   },
